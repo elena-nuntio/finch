f3c08bc356b798f19b187a017fc092ed2193934e
Zhedong Zheng <zhedongzheng@outlook.com>
2018-05-12 10:19:27 +0100

    minor change

--- nlp-models/tensorflow/end2end_mn/config.py
+++ nlp-models/tensorflow/end2end_mn/config.py
@@ -1,22 +1,11 @@
-"""
 import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--dropout_rate', type=float, default=0.3)
-parser.add_argument('--hidden_dim', type=int, default=64)
 parser.add_argument('--n_epochs', type=int, default=20)
 parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--dropout_rate', type=float, default=0.3)
+parser.add_argument('--hidden_dim', type=int, default=64)
+parser.add_argument('--n_hops', type=int, default=2)
 
 args = parser.parse_args()
-"""
-from bunch import Bunch
-
-args = Bunch({
-    'n_epochs': 20,
-    'batch_size': 64,
-    'hidden_dim': 64,
-    'dropout_rate': 0.3,
-    'n_hops': 2,
-    'clip_norm': 5.0,
-})
